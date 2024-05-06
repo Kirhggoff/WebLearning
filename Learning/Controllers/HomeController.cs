4326f6f31213762abc346d1b1c836b88fb4f914c
Aleksey <Aleksey@DESKTOP-K2DDIQ4>
2024-05-06 16:36:25 +0300

    Добавьте файлы проекта.

---
+++ Learning/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Learning.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Learning.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
